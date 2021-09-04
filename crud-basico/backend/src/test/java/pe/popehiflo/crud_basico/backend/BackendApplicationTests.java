46bab596d5de03151c1302d01e463537d9de10d4
popehiflo <poolpetterhijuel@hotmail.com>
2021-09-04 11:49:50 -0500

    Creacion del backend en springboot

---
+++ crud-basico/backend/src/test/java/pe/popehiflo/crud_basico/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.popehiflo.crud_basico.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
