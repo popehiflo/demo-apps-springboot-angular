929165bfd4177eccea670bfbcb26a8520621b07e
popehiflo <poolpetterhijuel@hotmail.com>
2021-09-05 22:26:21 -0500

    Repositorio y metodos para Producto

--- crud-basico/backend/src/main/java/pe/popehiflo/crud_basico/backend/repository/ProductoRepository.java
+++ crud-basico/backend/src/main/java/pe/popehiflo/crud_basico/backend/repository/ProductoRepository.java
@@ -3,10 +3,21 @@
  */
 package pe.popehiflo.crud_basico.backend.repository;
 
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import pe.popehiflo.crud_basico.backend.entity.Producto;
+
 /**
  * @author popehiflo
  *
  */
-public class ProductoRepository {
 
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto, Integer> {
+
+	Optional<Producto> findByProducto(String producto);
+	boolean existsByProducto(String producto);
 }
