307a638d7e410c49c5bcf2d043540209eba0c7e6
popehiflo <poolpetterhijuel@hotmail.com>
2021-09-05 22:33:13 -0500

    Added patron arq. DTO,multiples fuentes en una simple clase

--- crud-basico/backend/src/main/java/pe/popehiflo/crud_basico/backend/dto/Mensaje.java
+++ crud-basico/backend/src/main/java/pe/popehiflo/crud_basico/backend/dto/Mensaje.java
@@ -4,9 +4,32 @@
 package pe.popehiflo.crud_basico.backend.dto;
 
 /**
- * @author poolp
+ * @author popehiflo
  *
  */
 public class Mensaje {
 
+	private String mensaje;
+
+	/**
+	 * @param mensaje
+	 */
+	public Mensaje(String mensaje) {
+		this.mensaje = mensaje;
+	}
+
+	/**
+	 * @return the mensaje
+	 */
+	public String getMensaje() {
+		return mensaje;
+	}
+
+	/**
+	 * @param mensaje the mensaje to set
+	 */
+	public void setMensaje(String mensaje) {
+		this.mensaje = mensaje;
+	}
+	
 }
--- crud-basico/backend/src/main/java/pe/popehiflo/crud_basico/backend/dto/ProductoDto.java
+++ crud-basico/backend/src/main/java/pe/popehiflo/crud_basico/backend/dto/ProductoDto.java
@@ -3,10 +3,36 @@
  */
 package pe.popehiflo.crud_basico.backend.dto;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+
 /**
- * @author poolp
+ * @author popehiflo
  *
  */
+//@SuppressWarnings("deprecation")
 public class ProductoDto {
 
+	@NotBlank
+	private String producto;
+	
+	private String codigo;
+	@Min(0)
+	private float precio;
+	
+	public ProductoDto() {
+		
+	}
+	/**
+	 * @param producto
+	 * @param codigo
+	 * @param precio
+	 */
+	public ProductoDto(String producto, String codigo, float precio) {
+		super();
+		this.producto = producto;
+		this.codigo = codigo;
+		this.precio = precio;
+	}
+	
 }
