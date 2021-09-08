a48f7d8f1aa6533a91350f7732214e7111005bf5
popehiflo <poolpetterhijuel@hotmail.com>
2021-09-08 10:34:20 -0500

    Script base de datos

--- crud-basico/resources/script-database-create.sql
+++ crud-basico/resources/script-database-create.sql
@@ -21,12 +21,13 @@ USE `crud_basico`;
 -- -----------------------------------------------------
 -- Table `crud_basico`.`producto`
 -- -----------------------------------------------------
-create table producto (
-	id char(32) not null,
-	codigo char(8) not null unique,
-   producto varchar(150) not null,
+CREATE TABLE IF NOT EXISTS producto (
+	id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+	producto VARCHAR(150) NOT NULL,
+	codigo CHAR(8) NOT NULL UNIQUE,
+	precio DECIMAL(13,2) DEFAULT NULL,
    /*id_categoria_producto char(32) not null references categoria_producto(id),*/
-   primary key (id),
+   primary key (id)
    /*key fk_categoria_producto (id_categoria_producto),*/
    /*constraint fk_categoria_producto foreign key (id_categoria_producto) references categoria_producto (id)*/
 );
@@ -39,4 +40,6 @@ DROP USER IF EXISTS `crud_basico_user`;
 
 CREATE USER 'crud_basico_user'@'localhost' IDENTIFIED BY 'crud_basico_user';
 GRANT ALL PRIVILEGES ON crud_basico.* TO 'crud_basico_user'@'localhost';
+
+
 ALTER USER 'crud_basico_user'@'localhost' IDENTIFIED WITH mysql_native_password BY 'crud_basico_user-123';
