c9f994190b4d88ee498da60ec7ca0f366998d986
popehiflo <poolpetterhijuel@hotmail.com>
2021-09-08 16:05:06 -0500

    Added FormsModule y HttpClientModule

--- crud-basico/frontend/src/app/app.module.ts
+++ crud-basico/frontend/src/app/app.module.ts
@@ -2,18 +2,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ListaProductoComponent } from './producto/lista-producto.component';
+import { DetalleProductoComponent } from './producto/detalle-producto.component';
+import { NuevoProductoComponent } from './producto/nuevo-producto.component';
+import { EditarProductoComponent } from './producto/editar-producto.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 // external
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaProductoComponent,
+    DetalleProductoComponent,
+    NuevoProductoComponent,
+    EditarProductoComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule, // required animations module
     ToastrModule.forRoot(), // ToastrModule added
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
