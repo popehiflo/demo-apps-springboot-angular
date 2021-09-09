83d5bfc620fbc04d28d0ca5a1f3081294c3ba5e1
popehiflo <poolpetterhijuel@hotmail.com>
2021-09-08 23:47:33 -0500

    Added funcionalidad para agregar nuevo Producto

--- crud-basico/frontend/src/app/producto/nuevo-producto.component.ts
+++ crud-basico/frontend/src/app/producto/nuevo-producto.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductoService } from '../service/producto.service';
+import { Producto } from '../models/producto';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nuevo-producto',
@@ -7,9 +11,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NuevoProductoComponent implements OnInit {
 
-  constructor() { }
+  producto:string = '';
+  codigo:string = '';
+  precio:number = 0;
+
+  constructor(private productoService: ProductoService,
+              private toastr: ToastrService,
+              private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  onCreate():void {
+    const product = new Producto(this.producto, this.codigo, this.precio);
+    this.productoService.save(product).subscribe(
+      data => {
+        this.toastr.success('Producto creado!', 'OK', {
+          timeOut: 3000,
+          positionClass: 'toast-top-center'
+        });
+        this.router.navigate(['/']);
+      },
+      err => {
+        this.toastr.error(err.error.mensaje, 'Fail', {
+          timeOut: 3000,
+          positionClass: 'toast-top-center'
+        });
+
+      }
+
+    )
+    //this.router.navigate(['/']);
   }
 
 }
