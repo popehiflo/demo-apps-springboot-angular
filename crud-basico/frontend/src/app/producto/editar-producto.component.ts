060d0b2e7e83f4df6b530d249093702831c75f0e
popehiflo <poolpetterhijuel@hotmail.com>
2021-09-08 23:46:52 -0500

    Added funcionalidad de editar Producto

--- crud-basico/frontend/src/app/producto/editar-producto.component.ts
+++ crud-basico/frontend/src/app/producto/editar-producto.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Producto } from '../models/producto';
+import { ProductoService } from '../service/producto.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-editar-producto',
@@ -7,9 +11,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditarProductoComponent implements OnInit {
 
-  constructor() { }
+  producto:Producto = null;
+
+  constructor(
+    private productoService:ProductoService,
+    private activatedRoute:ActivatedRoute,
+    private toastr:ToastrService,
+    private router:Router
+  ) { }
 
   ngOnInit(): void {
+    const id = this.activatedRoute.snapshot.params.id;
+    this.productoService.detail(id).subscribe(
+      data => {
+        this.producto = data;
+      },
+      err => {
+        this.toastr.error(err.error.mensaje, 'Fail', {
+          timeOut: 3000,
+          positionClass: 'toast-top-center',
+        });
+        this.router.navigate(['/']);
+      }
+    );
+  }
+
+  onUpdate(): void {
+    const id = this.activatedRoute.snapshot.params.id;
+    this.productoService.update(id, this.producto).subscribe(
+      data => {
+        this.toastr.success('Producto actualizado', 'OK', {
+          timeOut: 3000,
+          positionClass: 'toast-top-center',
+        });
+        this.router.navigate(['/']);
+      },
+      err => {
+        this.toastr.error(err.error.mensaje, 'Fail', {
+          timeOut: 3000,
+          positionClass: 'toast-top-center',
+        });
+        //this.router.navigate(['/']);
+      }
+    );
   }
 
 }
